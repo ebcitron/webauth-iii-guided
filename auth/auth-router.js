0d288367055291e387f88816456d474f9642e0f6
Eli Citron <ebcitron@gmail.com>
2019-05-22 11:49:02 -0500

    Sanity Commit

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
-
+const jwt = require('jsonwebtoken');
 const Users = require('../users/users-model.js');
 
 // for endpoints beginning with /api/auth
@@ -25,9 +25,14 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+
+        // server needs to return the token to the client
+        // Isnt automatic, 
+        
         res.status(200).json({
-          message: `Welcome ${user.username}!`,
-        });
+          message: `Welcome ${user.username}!`, token
+       });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
       }
@@ -36,5 +41,19 @@ router.post('/login', (req, res) => {
       res.status(500).json(error);
     });
 });
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id, // sub in the payload is what the token is about
+    username: user.username,
+    // ... other data
+  };
+  const secret = "Its a secret";
+  const options = {
+    expiresIn: '1d', // 1d, 1day, 1h, 1hour
+  }
+
+  return jwt.sign(payload, secret, options);
+};
 
 module.exports = router;
--- index.js
+++ index.js
@@ -2,3 +2,18 @@ const server = require('./api/server.js');
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
+
+app.listen(, () => {
+    console.log('App listening on port 3000!');
+});
+
+app.listen(6660, () => {
+    console.log(`Server started on 6660`);
+});
+
+app.listen(3000, () => {
+    console.log('App listening on port 3000!');
+});
+
+
+APP.
